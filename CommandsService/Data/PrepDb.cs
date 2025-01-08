14f46b76375f6e5c5e745ab03f60131de90e1d29
helyousfi <visioncomputer200@gmail.com>
2025-01-08 23:45:37 +0100

    Changes added

---
+++ CommandsService/Data/PrepDb.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using CommandsService.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandsService.Data
+{
+    public static class PrepDb
+    {
+        
+    }
+}
